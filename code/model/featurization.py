8ceb3a5235d25006abbdde8d8085163b1ecfb4b3
Zhujun Xiao <zhujunxiao@Zhujuns-MacBook-Pro-2.local>
2019-10-27 17:50:21 -0500

    change path

--- code/model/featurization.py
+++ code/model/featurization.py
@@ -15,7 +15,7 @@ window_size = int(sys.argv[2])  # e.g. 100
 predict_size = int(sys.argv[3])  # e.g. 25
 normal_folder = str(sys.argv[4]) # e.g. ryerson
 anomaly_folder = str(sys.argv[5])  # e.g. 0208_anomaly
-anomaly_power = str(sys,argv[6])
+# anomaly_power = str(sys,argv[6])
 downsample_str = 'downsample_' + str(downsample_ratio)
 window_predict_size = str(sys.argv[2]) + '_' + str(sys.argv[3])
 
@@ -25,10 +25,10 @@ normal_input_path = path + 'downsample/{}/normal/{}/'.format(downsample_str,
                                                              normal_folder) 
 abnormal_input_path = path + 'downsample/{}/abnormal/{}/'.format(downsample_str, 
                                                                  anomaly_folder)
-normal_output_path = normal_input_path.replace('downsample', 'feature') \
+normal_output_path = normal_input_path.replace('/downsample/', '/feature/') \
                      + window_predict_size + '/'
  
-abnormal_output_path = abnormal_input_path.replace('downsample', 'feature') \
+abnormal_output_path = abnormal_input_path.replace('/downsample/', '/feature/') \
                      + window_predict_size + '/'
 normal_series_list_path = normal_output_path \
                           + 'normal_series_list_' + window_predict_size
@@ -47,16 +47,17 @@ if not os.path.exists(abnormal_output_path):
 ##########################################################
 print('start processing normal data....')
 for filename in sorted(glob.glob(normal_input_path + '*.txt')):
-    print(filename, out)
     out = normal_output_path + 'feature_' + os.path.basename(filename)
+    print(filename, out)
     utils.extract_method3(filename, open(out, 'w'), window_size, predict_size)
 
 
 
 print('start processing abnormal data....')
 for filename in sorted(glob.glob(abnormal_output_path + '*.txt')):
-    print(filename, out)
     out = normal_output_path + 'feature_' + os.path.basename(filename)
+    print(filename, out)
+
     utils.extract_method3(filename, open(out, 'w'), window_size, predict_size)
 
 
