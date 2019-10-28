67b9dc4acd23dc11bcbe8b56eaaa4547d10cdf00
Ziyu Ye <ziyuye@uchicago.edu>
2019-10-28 17:35:27 -0500

    Filter warnings!!

--- code/model/utils.py
+++ code/model/utils.py
@@ -3,7 +3,8 @@ Title: utils.py
 Prescription: Provide helper functions
 Declaration: Function `standard()` and `extract()` credit to Zhijing Li
 """
-
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import tensorflow as tf
 
