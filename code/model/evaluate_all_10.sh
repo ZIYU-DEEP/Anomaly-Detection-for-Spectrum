6492c240d88b8d47145ae5be9517c1bfa030c8fb
Ziyu Ye <ziyuye@uchicago.edu>
2019-10-29 09:16:04 -0500

    Change .sh

--- code/model/evaluate_all_10.sh
+++ code/model/evaluate_all_10.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-python featurization.py 100 1000 250 0208_anomaly abnormal
-python featurization.py 500 5000 1250 0208_anomaly abnormal
-python featurization.py 1000 10000 2500 0208_anomaly abnormal
-python featurization.py 2000 20000 5000 0208_anomaly abnormal
+python featurization.py 10 100 25 0208_anomaly abnormal
+python evaluation.py 10 100 25 ryerson 0208_anomaly 125 256 0
 
+python featurization.py 10 1000 250 0208_anomaly abnormal
+python evaluation.py 10 1000 250 ryerson 0208_anomaly 1250 256 0
 
-python evaluation.py 100 1000 250 ryerson 0208_anomaly 1250 256 0
-python evaluation.py 500 5000 1250 ryerson 0208_anomaly 6250 256 0
-python evaluation.py 1000 10000 2500 ryerson 0208_anomaly 12500 256 0
-python evaluation.py 2000 20000 5000 ryerson 0208_anomaly 25000 256 0
+python featurization.py 10 5000 1250 0208_anomaly abnormal
+python evaluation.py 10 5000 1250 ryerson 0208_anomaly 6250 256 0
+
+python featurization.py 10 10000 2500 0208_anomaly abnormal
+python evaluation.py 10 10000 2500 ryerson 0208_anomaly 12500 256 0
+
+python featurization.py 10 20000 5000 0208_anomaly abnormal
+python evaluation.py 10 20000 5000 ryerson 0208_anomaly 25000 256 0
--- code/model/run_first.sh
+++ code/model/run_first.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd ../preprocessing/
 
+python downsample_parse_data.py /net/adv_spectrum/data/raw/normal/ryerson 100
+python downsample_parse_data.py /net/adv_spectrum/data/raw/abnormal/0208_anomaly 100
+
 python downsample_parse_data.py /net/adv_spectrum/data/raw/normal/ryerson 500
 python downsample_parse_data.py /net/adv_spectrum/data/raw/abnormal/0208_anomaly 500
 
