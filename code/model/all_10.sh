6cc2bb31305afc50ced60781b5417deaba787b42
Zhujun Xiao <zhujunxiao@babygroot.cs.uchicago.edu>
2019-10-29 00:21:05 -0500

    change gpu number

--- code/model/all_10.sh
+++ code/model/all_10.sh
@@ -8,13 +8,13 @@ python training.py 10 1000 250 ryerson 0208_anomaly 250 1250 256 200 0
 python evaluation.py 10 1000 250 ryerson 0208_anomaly 1250 256 0
 
 python featurization.py 10 5000 1250 ryerson normal
-python training.py 10 5000 1250 ryerson 0208_anomaly 1250 6250 256 200 1
-python evaluation.py 10 5000 1250 ryerson 0208_anomaly 6250 256 1
+python training.py 10 5000 1250 ryerson 0208_anomaly 1250 6250 256 200 0
+python evaluation.py 10 5000 1250 ryerson 0208_anomaly 6250 256 0
 
 python featurization.py 10 10000 2500 ryerson normal
-python training.py 10 10000 2500 ryerson 0208_anomaly 2500 12500 256 200 2
-python evaluation.py 10 10000 2500 ryerson 0208_anomaly 12500 256 2
+python training.py 10 10000 2500 ryerson 0208_anomaly 2500 12500 256 200 0
+python evaluation.py 10 10000 2500 ryerson 0208_anomaly 12500 256 0
 
 python featurization.py 10 20000 5000 ryerson normal
-python training.py 10 20000 5000 ryerson 0208_anomaly 5000 25000 256 200 3
-python evaluation.py 10 20000 5000 ryerson 0208_anomaly 25000 256 3
+python training.py 10 20000 5000 ryerson 0208_anomaly 5000 25000 256 200 0
+python evaluation.py 10 20000 5000 ryerson 0208_anomaly 25000 256 0
