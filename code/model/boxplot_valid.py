6bf5ce8ed775bbbea87d920382dee149cbffd3a7
Ziyu Ye <ziyuye@uchicago.edu>
2019-11-01 18:00:06 -0500

    Draw boxplot

--- code/model/boxplot_valid.py
+++ code/model/boxplot_valid.py
@@ -3,25 +3,40 @@ Title: boxplot_valid.py
 Prescription: Draw the boxplot for multiple valid data.
 """
 
+import os
 import sys
 import glob
 import pickle
-import pandas
-import seaborn
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 ##########################################################
-# 1. Initialization
+# 1. Construct the List of Valid Error DataFrames
 ##########################################################
-# Arguments
-downsample_ratio = int(sys.argv[1])
-window_size = int(sys.argv[2])
-predict_size = int(sys.argv[3])
-normal_folder = str(sys.argv[4])  # e.g. ryerson
+path = '/net/adv_spectrum/result/error_df/valid/'
+base_df = pd.DataFrame()
+i = 0
+for filename in glob.glob(path + '**/**/*.pkl'):
+    with open(filename, 'rb') as f:
+        df = pickle.load(f)
+        basename = os.path.basename(filename)\
+                   .replace('valid_error_df_', '').replace('.pkl', '')
+        df.rename(columns={'valid_error': basename}, inplace=True)
+        if i == 0:
+            base_df = df
+        else:
+            base_df = pd.concat((base_df, df))
+        i += 1
 
-# String variables
-downsample_str = 'downsample_' + str(downsample_ratio)
-window_predict_size = str(sys.argv[2]) + '_' + str(sys.argv[3])
-
-valid_error_df_path = '/net/adv_spectrum/result/error_df/valid/' \
-                      '{}/{}/'.format(downsample_str, normal_folder)
-
+##########################################################
+# 2. Draw the Figure
+##########################################################
+sns.set_style('whitegrid')
+plt.figure(figsize=(20, 10))
+ax = sns.boxplot(data=base_df, showfliers=False, palette='Set2')
+ax.set_ylim(bottom=0, top=1.5)
+ax.yaxis.set_major_locator(ticker.MultipleLocator(0.15))
+sns.despine()
+ax.get_figure().savefig(path + 'boxplot_valid.png')
