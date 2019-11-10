279baee415936d5d63642d10f081cc0627ed66ae
Ziyu Ye <ziyuye@uchicago.edu>
2019-11-09 22:43:08 -0600

    no need -1

--- code/model/training.py
+++ code/model/training.py
@@ -32,7 +32,7 @@ gpu_no = int(sys.argv[9])
 # Set gpu environment
 # os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_no)
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_visible_devices(physical_devices[gpu_no - 1], 'GPU')
+tf.config.experimental.set_visible_devices(physical_devices[gpu_no], 'GPU')
 logical_devices = tf.config.experimental.list_logical_devices('GPU')
 
 # String variables
