7cebdb2f0cf792a3ac672a5cdc68496359f8f3dd
Shinan Liu <liushinan63@163.com>
2019-11-11 16:14:16 -0600

    only evaluate

--- code/model/ry2_jcl_mix.sh
+++ code/model/ry2_jcl_mix.sh
@@ -10,16 +10,16 @@
 #python ../preprocessing/downsample_parse_data.py /net/adv_spectrum/data/raw/abnormal/ry2_jcl_mix_G21/ 10
 #python ../preprocessing/downsample_parse_data.py /net/adv_spectrum/data/raw/abnormal/ry2_jcl_mix_G24/ 10
 
-python featurization.py 10 100 25 ry2_jcl_mix_G-3 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G0 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G3 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G6 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G9 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G12 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G15 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G18 abnormal
-python featurization.py 10 100 25 ry2_jcl_mix_G21 abnormal
-python featurization.py 10 100 25 rry2_jcl_mix_G24 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G-3 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G0 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G3 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G6 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G9 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G12 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G15 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G18 abnormal
+#python featurization.py 10 100 25 ry2_jcl_mix_G21 abnormal
+#python featurization.py 10 100 25 rry2_jcl_mix_G24 abnormal
 
 python evaluation_interval.py 10 100 25 ryerson_all ry2_jcl_mix_G-3 25 128 3
 python evaluation_interval.py 10 100 25 ryerson_all ry2_jcl_mix_G0 25 128 3
@@ -32,16 +32,16 @@ python evaluation_interval.py 10 100 25 ryerson_all ry2_jcl_mix_G18 25 128 3
 python evaluation_interval.py 10 100 25 ryerson_all ry2_jcl_mix_G21 25 128 3
 python evaluation_interval.py 10 100 25 ryerson_all ry2_jcl_mix_G24 25 128 3
 
-python featurization.py 10 1000 250 ry2_jcl_mix_G-3 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G0 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G3 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G6 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G9 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G12 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G15 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G18 abnormal
-python featurization.py 10 1000 250 ry2_jcl_mix_G21 abnormal
-python featurization.py 10 1000 250 rry2_jcl_mix_G24 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G-3 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G0 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G3 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G6 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G9 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G12 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G15 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G18 abnormal
+#python featurization.py 10 1000 250 ry2_jcl_mix_G21 abnormal
+#python featurization.py 10 1000 250 rry2_jcl_mix_G24 abnormal
 
 python evaluation_interval.py 10 1000 250 ryerson_all ry2_jcl_mix_G-3 250 128 3
 python evaluation_interval.py 10 1000 250 ryerson_all ry2_jcl_mix_G0 250 128 3
