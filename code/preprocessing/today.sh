2da751d0aedaa32801ab99c8e1422195e41830e5
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-04 22:33:55 -0600

    Update today.sh

--- code/preprocessing/today.sh
+++ code/preprocessing/today.sh
@@ -1,9 +1,6 @@
-python create_squeeze_array.py abnormal ryerson_ab_train_LOS-5M-USRP2
-python create_squeeze_array.py abnormal ryerson_ab_train_LOS-5M-USRP3
-python create_squeeze_array.py abnormal ryerson_ab_train_NLOS-5M-USRP1
-python create_squeeze_array.py abnormal ryerson_ab_train_Dynamics-5M-USRP1
+python spec2rss.py ryerson_ab_train_LOS-5M-USRP1 abnormal
+python spec2rss.py ryerson_ab_train_LOS-5M-USRP2 abnormal
+python spec2rss.py ryerson_ab_train_LOS-5M-USRP3 abnormal
+python spec2rss.py ryerson_ab_train_NLOS-5M-USRP1 abnormal
+python spec2rss.py ryerson_ab_train_Dynamics-5M-USRP1 abnormal
 
-python main_evaluate.py sigOver_319 LOS-5M-USRP2_1599 0.05
-python main_evaluate.py sigOver_319 LOS-5M-USRP3_1599 0.05
-python main_evaluate.py sigOver_319 NLOS-5M-USRP1_1599 0.05
-python main_evaluate.py sigOver_319 Dynamics-5M-USRP1_1599 0.05
