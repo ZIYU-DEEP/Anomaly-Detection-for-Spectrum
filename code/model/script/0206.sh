1217ede2261235b6e55469eafd6e21493be7cd87
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-06 11:26:13 -0600

    Update 0206.sh

--- code/model/script/0206.sh
+++ code/model/script/0206.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd ~/Anomaly-Detection-for-Spectrum/code/model
 
-python featurization.py 10 100 25 ryerson_train normal
+python featurization.py 10 100 25 ryerson_train normal 8
 python training_joblib.py 10 100 25 ryerson_train 25 125 128 266 1
+
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_LOS-5M-USRP1 125 128 1
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_LOS-5M-USRP2 125 128 1
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_LOS-5M-USRP3 125 128 1
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_Dynamics-5M-USRP1 125 128 1
 chmod -R 777 .
