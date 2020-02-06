d8869f613b01f7178b4529328fcb95ba62c6a30b
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-06 11:23:40 -0600

    Update today.sh

--- code/model/script/today.sh
+++ code/model/script/today.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/Anomaly-Detection-for-Spectrum/code/model/
 
-python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_LOS-5M-USRP2 1250 128 0
-python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_LOS-5M-USRP3 1250 128 0
-python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_Dynamics-5M-USRP1 1250 128 0
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_LOS-5M-USRP1 125 128 1
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_LOS-5M-USRP2 125 128 1
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_LOS-5M-USRP3 125 128 1
+python evaluation_joblib.py 10 100 25 ryerson_train ryerson_ab_train_Dynamics-5M-USRP1 125 128 1
