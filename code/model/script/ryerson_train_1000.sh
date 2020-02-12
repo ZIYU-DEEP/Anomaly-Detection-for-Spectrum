bd906b5eb347fb43d4627281a1f4bebc3c15d1d2
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-12 07:49:29 -0600

    Update and rename only_fbs_1000.sh to ryerson_train_1000.sh

---
+++ code/model/script/ryerson_train_1000.sh
@@ -0,0 +1,15 @@
+  
+cd ~/Anomaly-Detection-for-Spectrum/code/model
+
+
+python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_LOS-5M-USRP1 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_LOS-5M-USRP2 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_LOS-5M-USRP3 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_NLOS-5M-USRP1 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson_train ryerson_ab_train_Dynamics-5M-USRP1 1250 128 0
+
+cd /net/adv_spectrum/
