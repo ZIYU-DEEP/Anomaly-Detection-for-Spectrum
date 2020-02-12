2beba431271a515a0319ed984671c8006f83cae4
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-12 10:18:28 -0600

    Update and rename JCL_10.sh to 871_to_rss.sh

---
+++ code/model/script/871_to_rss.sh
@@ -0,0 +1,7 @@
+cd ~
+python spec2rss.py 871 normal
+python spec2rss.py 871_ab_LOS-5M-USRP1 abnormal
+python spec2rss.py 871_ab_LOS-5M-USRP2 abnormal
+python spec2rss.py 871_ab_LOS-5M-USRP3 abnormal
+python spec2rss.py 871_ab_NLOS-5M-USRP1 abnormal
+python spec2rss.py 871_ab_Dynamics-5M-USRP1 abnormal
