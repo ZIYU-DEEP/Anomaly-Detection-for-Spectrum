81999ff2fa798367f84766d9983376a2c9c64798
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-12 14:50:58 -0600

    Update and rename evaluate_nov12.sh to downtown_train_eval.sh

---
+++ code/model/script/downtown_train_eval.sh
@@ -0,0 +1,14 @@
+python training.py 10 100 25 downtown 25 125 256 300 3
+python training.py 10 1000 250 downtown 250 1250 256 300 3
+
+python evaluation_joblib.py 10 100 25 downtown downtown_ab_LOS-5M-USRP1 125 128 3
+python evaluation_joblib.py 10 100 25 downtown downtown_ab_LOS-5M-USRP2 125 128 3
+python evaluation_joblib.py 10 100 25 downtown downtown_ab_LOS-5M-USRP3 125 128 3
+python evaluation_joblib.py 10 100 25 downtown downtown_ab_NLOS-5M-USRP1 125 128 3
+python evaluation_joblib.py 10 100 25 downtown downtown_ab_Dynamics-5M-USRP1 125 128 3
+
+python evaluation_joblib.py 10 1000 250 downtown downtown_ab_LOS-5M-USRP1 1250 128 3
+python evaluation_joblib.py 10 1000 250 downtown downtown_ab_LOS-5M-USRP2 1250 128 3
+python evaluation_joblib.py 10 1000 250 downtown downtown_ab_LOS-5M-USRP3 1250 128 3
+python evaluation_joblib.py 10 1000 250 downtown downtown_ab_NLOS-5M-USRP1 1250 128 3
+python evaluation_joblib.py 10 1000 250 downtown downtown_ab_Dynamics-5M-USRP1 1250 128 3
