cc27b83569367eb42bb3082eb8d002d519d855c5
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-11 22:45:25 -0600

    Update eval_871.sh

--- code/model/script/eval_871.sh
+++ code/model/script/eval_871.sh
@@ -1,14 +1,14 @@
 cd ~/Anomaly-Detection-for-Spectrum/code/model
 
-python evaluation_joblib.py 10 1000 250 871 871_ab_LOS-5M-USRP2 1250 128 2
-python evaluation_joblib.py 10 1000 250 871 871_ab_LOS-5M-USRP3 1250 128 2
-python evaluation_joblib.py 10 1000 250 871 871_ab_NLOS-5M-USRP1 1250 128 2
-python evaluation_joblib.py 10 1000 250 871 871_ab_Dynamics-5M-USRP1 1250 128 2
+python evaluation_joblib.py 10 1000 250 871 871_ab_LOS-5M-USRP2 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_LOS-5M-USRP3 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_NLOS-5M-USRP1 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_Dynamics-5M-USRP1 1250 128 0
 
-python evaluation_joblib.py 10 100 25 871 871_ab_LOS-5M-USRP2 125 128 2
-python evaluation_joblib.py 10 100 25 871 871_ab_LOS-5M-USRP3 125 128 2
-python evaluation_joblib.py 10 100 25 871 871_ab_NLOS-5M-USRP1 125 128 2
-python evaluation_joblib.py 10 1000 25 871 871_ab_Dynamics-5M-USRP1 125 128 2
+python evaluation_joblib.py 10 100 25 871 871_ab_LOS-5M-USRP2 125 128 0
+python evaluation_joblib.py 10 100 25 871 871_ab_LOS-5M-USRP3 125 128 0
+python evaluation_joblib.py 10 100 25 871 871_ab_NLOS-5M-USRP1 125 128 0
+python evaluation_joblib.py 10 1000 25 871 871_ab_Dynamics-5M-USRP1 125 128 0
 
 cd /net/adv_spectrum/
 chmod -R 777 .
