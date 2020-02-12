1267b7ffad99fa8b4d0a95cd4d6bb223245c7b56
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-12 14:51:30 -0600

    Update downtown_train_eval.sh

--- code/model/script/downtown_train_eval.sh
+++ code/model/script/downtown_train_eval.sh
@@ -1,3 +1,4 @@
+cd ..
 python training.py 10 100 25 downtown 25 125 256 300 3
 python training.py 10 1000 250 downtown 250 1250 256 300 3
 
@@ -12,3 +13,6 @@ python evaluation_joblib.py 10 1000 250 downtown downtown_ab_LOS-5M-USRP2 1250 1
 python evaluation_joblib.py 10 1000 250 downtown downtown_ab_LOS-5M-USRP3 1250 128 3
 python evaluation_joblib.py 10 1000 250 downtown downtown_ab_NLOS-5M-USRP1 1250 128 3
 python evaluation_joblib.py 10 1000 250 downtown downtown_ab_Dynamics-5M-USRP1 1250 128 3
+
+cd /net/adv_spectrum/
+chmod -R 777 .
