90a5676ebcfa55da3f59332597fd65e60f331fdf
Shinan Liu <shinanliu@babygroot.cs.uchicago.edu>
2020-02-13 10:38:15 -0600

    fix campus_drive

--- code/model/script/campus_drive/campus_drive_add.sh
+++ code/model/script/campus_drive/campus_drive_add.sh
@@ -4,11 +4,12 @@ b="campus_drive_"
 a+=$1
 echo $a
 python add_raw.py 6 $1 6 5
-python downsample_parse_data.py $a 6 5
+python downsample_parse_data.py $a 10 5
 rm -rf $a
 
 cd ../model
 b+=$1
 echo $b
+python featurization.py 10 100 25 $b abnormal 5
 python featurization.py 10 1000 250 $b abnormal 5
 chmod 777 -R /net/adv_spectrum
--- code/model/script/campus_drive/campus_train_eval.sh
+++ code/model/script/campus_drive/campus_train_eval.sh
@@ -1,6 +1,6 @@
-cd ..
-python training.py 10 100 25 campus_drive 25 125 256 300 3
-python training.py 10 1000 250 campus_drive 250 1250 256 300 3
+cd ../../
+python training_joblib.py 10 100 25 campus_drive 25 125 256 300 3
+python training_joblib.py 10 1000 250 campus_drive 250 1250 256 300 3
 
 python evaluation_joblib.py 10 100 25 campus_drive campus_drive_LOS-5M-USRP1 125 128 3
 python evaluation_joblib.py 10 100 25 campus_drive campus_drive_LOS-5M-USRP2 125 128 3
