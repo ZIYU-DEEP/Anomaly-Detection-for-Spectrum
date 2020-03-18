efcc1532da8db82a64f90aca07720a2d5e1f09d0
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-17 22:30:09 -0500

    delete os.environment

--- code/model/featurization.py
+++ code/model/featurization.py
@@ -12,7 +12,6 @@ import numpy as np
 import multiprocessing as mp
 
 warnings.filterwarnings('ignore')
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 ##########################################################
