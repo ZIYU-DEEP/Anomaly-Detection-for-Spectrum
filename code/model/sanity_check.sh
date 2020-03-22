a8ac6a6948eb3857056d9d690a3ceab1bbcc20ed
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-22 11:44:29 -0500

    Add files via upload

---
+++ code/model/sanity_check.sh
@@ -0,0 +1,44 @@
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_wn_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_wn_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_fsk_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_fsk_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_psk_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_psk_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_qam_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_qam_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_ofdm_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 ryerson ryerson_ab_train_ofdm_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 871 871_ab_wn_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_wn_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 871 871_ab_fsk_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_fsk_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 871 871_ab_qam_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_qam_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 871 871_ab_ofdm_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_ofdm_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 871 871_ab_psk_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 871 871_ab_psk_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 downtown downtown_wn_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 downtown downtown_wn_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 downtown downtown_fsk_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 downtown downtown_fsk_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 downtown downtown_qam_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 downtown downtown_qam_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 downtown downtown_ofdm_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 downtown downtown_ofdm_5G 1250 128 0
+
+python evaluation_joblib.py 10 1000 250 downtown downtown_psk_1.4G 1250 128 0
+python evaluation_joblib.py 10 1000 250 downtown downtown_psk_5G 1250 128 0
